8cab9fa57dbe205b13e8e62c9f4052ff343cc978
joshaurellano <jaaurellano@gbox.ncf.edu.ph>
2026-01-28 09:18:47 +0800

    Added initial homescreen layout

---
+++ lib/question_model.dart
@@ -0,0 +1,11 @@
+class Question{
+  final String questionText;
+  final List<String> options;
+  final int correctAnswer;
+
+  Question({
+    required this.questionText,
+    required this.options,
+    required this.correctAnswer,
+  });
+}
